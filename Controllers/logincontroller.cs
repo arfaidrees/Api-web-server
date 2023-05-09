448d0803c8e3a27fa87a50111ba6cc7aad6f9298
arfaa idrees <19101001-067@uskt.edu.pk>
2023-05-10 00:46:57 +0500

    updated

---
+++ Controllers/logincontroller.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_assignment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class logincontroller : ControllerBase
+    {
+        private readonly AppDbcontext 
+        public logincontroller(AppDbcontext appdbcontext)
+            [HttpGet]
+        [Route("api/getdata/{id}")]
+        public async Task<IActionResult> GetData(int id)
+        {
+            var result = await AppDbcontext.GetDataAsync(id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+        [HttpPost]
+        [Route("api/postdata")]
+        public async Task<IActionResult> PostData([FromBody] DataModel data)
+        {
+            if (data == null)
+            {
+                return BadRequest();
+            }
+            var result = await AppDbcontext.SaveDataAsync(data);
+            if (result == null)
+            {
+                return BadRequest();
+            }
+            return CreatedAtAction(nameof(GetData), new { id = result.Id }, result);
+        }
+
+    }
+}
+
--- model/login.cs
+++ model/login.cs
@@ -6,9 +6,9 @@ namespace api_assignment.model
     {
         [Key]
 
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+    
+        public string uname { get; set; }
+        public string upass { get; set; }
 
     }
 }
